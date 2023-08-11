069a9409ffc831db7a63096ba1a2bcd62b91e6ed
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-08-11 07:30:11 +0000

    3.16 Complete

--- index.js
+++ index.js
@@ -70,14 +70,10 @@ app.get('/api/persons/:id', (request,response) => {
     }))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => response.status(204).end())
-    .catch(error => response.status(404).send({error: 'malformatted Id'}))
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -103,6 +99,18 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => response.json(savedPerson))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted Id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT= process.env.PORT || 3000
 app.listen(PORT, () => {
